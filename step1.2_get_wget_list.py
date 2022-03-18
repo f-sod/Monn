d470712d14ad8a7f18c7cdb729b8271ef3c0b5a1
f-sod <98804415+f-sod@users.noreply.github.com>
2022-03-18 11:26:51 +0100

    Add files via upload

--- step1.2_get_wget_list.py
+++ step1.2_get_wget_list.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 
-# =================================== protein-ligand complex =======================
+__auteurs__ = ""
+__update_by__="ODJE Floriane"
+__data__ = "2022-03"
+
+
 def get_pdbid_list():
+	""" 
+	Extract PDB IDs of all protein-ligand complex
+	While parsing INDEX_general_PL.2020
+
+	Return
+	------
+		list : pdbid_list of 19443 elements 
+	"""
 	pdbid_list = []
-	with open('./pdbbind_index/INDEX_general_PL.2020') as f:
-		for line in f.readlines():
+	with open('./pdbbind_index/INDEX_general_PL.2020') as f1:
+		for line in f1.readlines():
 			if line[0] != '#':
 				pdbid_list.append(line.strip().split()[0])
 	print(f"pdbid_list,{len(pdbid_list)}")
 	return pdbid_list
 pdbid_list = get_pdbid_list()
 
-with open('out1.2_pdbid_list.txt','w') as fw:
-	for pdbid in pdbid_list:
-		fw.write(pdbid+'\n')
 
-
-# creation d'un fichie de sortie des liens pour obtenir les complexes des pdb ID
-with open('out1.2_pdbbind_wget_complex.txt','w') as fw:
-	for pdbid in pdbid_list:
-		fw.write('https://files.rcsb.org/download/'+pdbid+'.pdb\n')
-
-# ================================= ligand pdb file =================================
+# ligand pdb file 
 def get_pdbid_to_ligand():
+	"""
+	Extract ligand IDs of all protein-ligand complex
+	While parsing INDEX_general_PL.2020
+
+	Return
+	------
+		List: pdbid_to_ligand of 16726 elements 
+	"""
 	pdbid_to_ligand = {}
 	with open('./pdbbind_index/INDEX_general_PL.2020') as f:
 		for line in f.readlines():
@@ -37,11 +48,22 @@ def get_pdbid_to_ligand():
 				pdbid_to_ligand[line[:4]] = ligand
 	print(f"pdbid_to_ligand,{len(pdbid_to_ligand)}")
 	return pdbid_to_ligand
-pdbid_to_ligand = get_pdbid_to_ligand()
 
-# creation d'un fichier de sortie des liens des ligands retrouve dans les complexes
-with open('out1.2_pdbbind_wget_ligand.txt','w') as fw : 
-	for pdbid in pdbid_list:
-		if pdbid in pdbid_to_ligand:
-			ligand = pdbid_to_ligand[pdbid]
-			fw.write('https://files.rcsb.org/ligands/download/'+ligand+'_ideal.pdb\n')
+if __name__ == "__main__":
+	
+	pdbid_to_ligand = get_pdbid_to_ligand()
+	#Creation of downloadable file containing ligand links and pdb file 
+	with open('out1.2_pdbbind_wget_ligand.txt','w') as fw : 
+		for pdbid in pdbid_list:
+			if pdbid in pdbid_to_ligand:
+				ligand = pdbid_to_ligand[pdbid]
+				fw.write('https://files.rcsb.org/ligands/download/'+ligand+'_ideal.pdb\n')
+
+	with open('out1.2_pdbbind_wget_complex.txt','w') as f1w:
+		for pdbid in pdbid_list:
+			f1w.write('https://files.rcsb.org/download/'+pdbid+'.pdb\n')
+	
+	#PDB code list file creation 
+	with open('out1.2_pdbid_list.txt','w') as f1w:
+		for pdbid in pdbid_list:
+			f1w.write(pdbid+'\n')
